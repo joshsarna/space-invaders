5f2bf8d7ff75049756cb0e41f61f840370e6a8e5
joshsarna <joshsarna@gmail.com>
2019-05-03 23:29:59 -0700

    set light gray background color

--- alient_invasion.py
+++ alient_invasion.py
@@ -8,6 +8,9 @@ def run_game():
   screen = pygame.display.set_mode((1200, 800))
   pygame.display.set_caption("Alien Invasion")
 
+  # set the background color
+  bg_color = (230, 230, 230)
+
   # start the main loop for the game
   while True:
     # watch for keyboard and mouse events
@@ -15,7 +18,10 @@ def run_game():
       if event.type == pygame.QUIT:
         sys.exit()
 
-    # mkae the most recently drawn screen visible
+    # redraw the screen during each pass through the loop
+    screen.fill(bg_color)
+
+    # make the most recently drawn screen visible
     pygame.display.flip()
 
 run_game()
